526c2020c6070088ece14d1e6eaa043d905d196b
Dor Harpaz <harpazdor@gmail.com>
2022-07-16 00:08:04 +0300

    Decorate url_to_bs

--- utils_web.py
+++ utils_web.py
@@ -33,6 +33,17 @@ def get_bs(url):
 	bs = BeautifulSoup(req.content, features="lxml")
 	return bs
 
+def url_to_bs(func):
+	def inner(url, *args, **kwargs):
+		bs = get_bs(url)
+
+		# in case the request failed.
+		if bs is None:
+			return None
+
+		return func(bs, *args, **kwargs)
+	return inner
+
 #
 # The exported function
 #
@@ -49,25 +60,15 @@ def read_link(url):
 		else:
 			return read_link_other(url)
 
-def read_link_reddit():
-	bs = get_bs(url)
-
-	# in case the request failed.
-	if bs is None:
-		return None
-
+@url_to_bs
+def read_link_reddit(bs):
 	# from my manual test, bs.find_all("h1") brings two identical results.
 	post_name = bs.find("h1").text
 
 	return post_name
 
-def read_link_youtube(url):
-	bs = get_bs(url)
-
-	# in case the request failed.
-	if bs is None:
-		return None
-
+@url_to_bs
+def read_link_youtube(bs):
 	# it should return "<video name> - YouTube"
 	web_page_title = bs.find("title").text
 
@@ -76,13 +77,8 @@ def read_link_youtube(url):
 def read_link_pdf(url):
 	return unquote(url).split('/')[-1]
 
-def read_link_other(url):
-	bs = get_bs(url)
-
-	# in case the request failed.
-	if bs is None:
-		return None
-
+@url_to_bs
+def read_link_other(bs):
 	title = bs.find('title').text
 	log(f".....[*] Setting title: {title}")
 	return title
