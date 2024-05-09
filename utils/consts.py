3952ef2822821122917a2ffeabbb2451739a5cf7
Dor Harpaz <harpazdor@gmail.com>
2024-05-09 13:40:36 +0300

    remove organization leftovers

--- utils/consts.py
+++ utils/consts.py
@@ -1,28 +1,3 @@
 import os
 
 MAIN_FOLDER = os.path.dirname(os.path.dirname(__file__))
-
-# key is common-name, value is trello-name
-ORGANIZATIONS = {
-	"Cooking"                         : "cooking220"             ,
-	"Personal Lists"                  : "personallists51"        ,
-	"Personal Lists Backlog"          : "personallistsbacklog"   ,
-	"Reading"                         : "reading152"             ,
-	"Reading - Backlog"               : "readingbacklog"         ,
-	"Reading - Done"                  : "readingdone"            ,
-	"Reading - Lists (archive)"       : "readinglists17"         ,
-	"Reading - Wont do"               : "readingwontdo"          ,
-	"Y - Quantum Computing"           : "quantumcomputing13"     ,
-	"Y - Quantum Computing - Backlog" : "quantumcomputingbacklog",
-	"Y - Quantum Computing - Done"    : "quantumcomputingdone"   ,
-	"Z_Learn_Old"                     : "z_learn_old"            ,
-	"Z_Old"                           : "old355"                 ,
-	"Z_Old_2"                         : "zz_old"                 ,
-	"Z_Old_Projects"                  : "z_old_3"                ,
-	"ZZZ_2022.09.19_Apartment"        : "apartment45"            ,
-	"ZZZ_2022.09.19_Blog"             : "blog75891337"           ,
-	"ZZZ_2022.09.19_Family"           : "family29589917"         ,
-	"ZZZ_2022.09.19_Personal"         : "personal67443136"       ,
-	"ZZZ_2022.09.19_Projects"         : "projects04437456"       ,
-}
-ORGANIZATIONS_REVERSE = { v: k for k,v in ORGANIZATIONS.items() }
